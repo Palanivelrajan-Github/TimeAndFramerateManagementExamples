3b8a04918671d66355082a695183f2fec56e263f
rohan29github <72843486+rohan29github@users.noreply.github.com>
2021-02-20 11:37:36 +0530

    Time.deltaTime

--- Assets/Scripts/TimeDeltaTime.cs
+++ Assets/Scripts/TimeDeltaTime.cs
@@ -1,10 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class TimeDeltaTime : MonoBehaviour
-{
-    // Time.deltaTime example.
+// Time.deltaTime example.
 //
 // Wait two seconds and display waited time.
 // This is typically just beyond 2 seconds.
@@ -12,66 +8,64 @@ public class TimeDeltaTime : MonoBehaviour
 // It can range between 0.5 and 2.0. These changes only
 // happen when the timer restarts.
 
-    public class ScriptExample : MonoBehaviour
-    {
-        private float scrollBar = 1.0f;
-        private float timer;
-        private float value = 10.0f;
-        private float visualTime;
-        private readonly float waitTime = 2.0f;
-        private int width, height;
+public class TimeDeltaTime : MonoBehaviour
+{
+    private float scrollBar = 1.0f;
+    private float timer;
+    private float value = 10.0f;
+    private float visualTime;
+    private readonly float waitTime = 2.0f;
+    private int width, height;
 
-        private void Awake()
+    private void Awake()
+    {
+        width = Screen.width;
+        height = Screen.height;
+        Time.timeScale = scrollBar;
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        // Check if we have reached beyond 2 seconds.
+        // Subtracting two is more accurate over time than resetting to zero.
+        if (timer > waitTime)
         {
-            width = Screen.width;
-            height = Screen.height;
+            visualTime = timer;
+
+            // Remove the recorded 2 seconds.
+            timer = timer - waitTime;
             Time.timeScale = scrollBar;
         }
+    }
 
-        private void Update()
-        {
-            timer += Time.deltaTime;
+    private void OnGUI()
+    {
+        var sliderDetails = new GUIStyle(GUI.skin.GetStyle("horizontalSlider"));
+        var sliderThumbDetails = new GUIStyle(GUI.skin.GetStyle("horizontalSliderThumb"));
+        var labelDetails = new GUIStyle(GUI.skin.GetStyle("label"));
 
-            // Check if we have reached beyond 2 seconds.
-            // Subtracting two is more accurate over time than resetting to zero.
-            if (timer > waitTime)
-            {
-                visualTime = timer;
+        // Set the size of the fonts and the width/height of the slider.
+        labelDetails.fontSize = 6 * (width / 200);
+        sliderDetails.fixedHeight = height / 32;
+        sliderDetails.fontSize = 12 * (width / 200);
+        sliderThumbDetails.fixedHeight = height / 32;
+        sliderThumbDetails.fixedWidth = width / 32;
 
-                // Remove the recorded 2 seconds.
-                timer = timer - waitTime;
-                Time.timeScale = scrollBar;
-            }
-        }
+        // Show the slider. Make the scale to be ten times bigger than the needed size.
+        value = GUI.HorizontalSlider(new Rect(width / 8, height / 4, width - 4 * width / 8, height - 2 * height / 4),
+            value, 5.0f, 20.0f, sliderDetails, sliderThumbDetails);
 
-        private void OnGUI()
-        {
-            var sliderDetails = new GUIStyle(GUI.skin.GetStyle("horizontalSlider"));
-            var sliderThumbDetails = new GUIStyle(GUI.skin.GetStyle("horizontalSliderThumb"));
-            var labelDetails = new GUIStyle(GUI.skin.GetStyle("label"));
+        // Show the value from the slider. Make sure that 0.5, 0.6... 1.9, 2.0 are shown.
+        var v = Mathf.RoundToInt(value) / 10.0f;
+        GUI.Label(new Rect(width / 8, height / 3.25f, width - 2 * width / 8, height - 2 * height / 4),
+            "timeScale: " + v.ToString("f1"), labelDetails);
+        scrollBar = v;
 
-            // Set the size of the fonts and the width/height of the slider.
-            labelDetails.fontSize = 6 * (width / 200);
-            sliderDetails.fixedHeight = height / 32;
-            sliderDetails.fontSize = 12 * (width / 200);
-            sliderThumbDetails.fixedHeight = height / 32;
-            sliderThumbDetails.fixedWidth = width / 32;
-
-            // Show the slider. Make the scale to be ten times bigger than the needed size.
-            value = GUI.HorizontalSlider(
-                new Rect(width / 8, height / 4, width - 4 * width / 8, height - 2 * height / 4),
-                value, 5.0f, 20.0f, sliderDetails, sliderThumbDetails);
-
-            // Show the value from the slider. Make sure that 0.5, 0.6... 1.9, 2.0 are shown.
-            var v = Mathf.RoundToInt(value) / 10.0f;
-            GUI.Label(new Rect(width / 8, height / 3.25f, width - 2 * width / 8, height - 2 * height / 4),
-                "timeScale: " + v.ToString("f1"), labelDetails);
-            scrollBar = v;
-
-            // Display the recorded time in a certain size.
-            labelDetails.fontSize = 14 * (width / 200);
-            GUI.Label(new Rect(width / 8, height / 2, width - 2 * width / 8, height - 2 * height / 4),
-                "Timer value is: " + visualTime.ToString("f4") + " seconds.", labelDetails);
-        }
+        // Display the recorded time in a certain size.
+        labelDetails.fontSize = 14 * (width / 200);
+        GUI.Label(new Rect(width / 8, height / 2, width - 2 * width / 8, height - 2 * height / 4),
+            "Timer value is: " + visualTime.ToString("f4") + " seconds.", labelDetails);
     }
 }
