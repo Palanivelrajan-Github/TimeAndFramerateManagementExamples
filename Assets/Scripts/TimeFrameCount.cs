8b1142c35be48ae2624adf7b1e346397f44f36d8
rohan29github <72843486+rohan29github@users.noreply.github.com>
2021-03-01 13:13:41 +0530

    TimeFrameCount class upated

--- Assets/Scripts/TimeFrameCount.cs
+++ Assets/Scripts/TimeFrameCount.cs
@@ -5,8 +5,14 @@ public class TimeFrameCount : MonoBehaviour
     // Start is called before the first frame update
 
     // Update is called once per frame
+    private float _secTimer;
     void Update()
     {
-        Debug.Log(Time.frameCount);
+        _secTimer += Time.deltaTime;
+        if (_secTimer > 1.0f)
+        {
+            Debug.Log(Time.frameCount);
+            _secTimer = 0.0f;
+        }
     }
 }
