0879542d93fabb7dfbe464545e2a4038adc108bb
rohan29github <72843486+rohan29github@users.noreply.github.com>
2021-03-02 11:26:00 +0530

    timer class commit again

--- Assets/Scripts/TimeMaximumParticleDeltaTime.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class TimeMaximumParticleDeltaTime : MonoBehaviour
-{
-    private void OnParticleUpdateJobScheduled()
-    {
-        Debug.Log(Time.maximumParticleDeltaTime);
-    }
-}
